5b9146ec41cd3e2e86296f14daac5cf34509d79f
prabs.kittu@gmail.com <prabs.kittu@gmail.com>
2021-08-01 18:05:14 +0000

    prabs-test

--- models/fact/fct_cust_pymnt.sql
+++ models/fact/fct_cust_pymnt.sql
@@ -3,15 +3,13 @@ payments as (select * from {{ ref('stg_payments')}}),
 
 final_amount as (
     select
-        customer_id,
-
         order_id,
         sum(amount) as lifetime_value
        
     from orders 
 
-    inner join payments using (customer_id,order_id)
-    group by 1,2
+    inner join payments using (order_id)
+    group by 1
 
 )
 
