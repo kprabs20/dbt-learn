691b92237909d0e8c14d20db44eb9a46c7169de9
prabs.kittu@gmail.com <prabs.kittu@gmail.com>
2021-08-01 18:11:51 +0000

    prabs-test

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,5 +1,11 @@
-select 
-       orderid as order_id,
-       sum(amount) as amount
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
 from demo_db.stripe.payment 
-group by 1
