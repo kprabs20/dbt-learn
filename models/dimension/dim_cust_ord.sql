2680ff313a67653e91a6fbb8a03a48eb0eb46f98
prabs.kittu@gmail.com <prabs.kittu@gmail.com>
2021-08-01 18:26:49 +0000

    prabs-test

--- models/dimension/dim_cust_ord.sql
+++ models/dimension/dim_cust_ord.sql
@@ -1,7 +1,5 @@
 with customers as (select * from {{ ref('stg_cust') }}),
 
-customer_payment as (select * from {{ ref('fct_cust_pymnt') }}),
-
 customer_orders as (select * from {{ ref('int_custord')}}),
 
 
@@ -15,14 +13,12 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(customer_payment.lifetime_value,0) as final_payment_amount
+        customer_orders.lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
 
-    left join customer_payment using (customer_id)
-
-)
+    )
 
 select * from final
